f093094138aa7a789db0096057be819543ff301e
Salam Salehpour <a.salehpour@tosan.com>
2021-01-21 01:06:29 +0330

    Print my name in standard output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleProgramC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Salam Salehpour");
         }
     }
 }
